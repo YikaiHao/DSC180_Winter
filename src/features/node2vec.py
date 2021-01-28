85061de77c8fe5cbb7745520b3a736afbc7fd604
yuc399 <yuc399@ucsd.edu>
2021-01-28 09:54:26 -0800

    update node2vec

--- src/features/node2vec.py
+++ src/features/node2vec.py
@@ -35,10 +35,10 @@ class Node2Vec():
         if 'B' not in self.mat_dict:
             for _ in tqdm(range(self.num_sentences)):  
                 sentence_len = np.random.choice(self.num_tokens)
-                api_row = self.A_train.shape[0]
-                app_col = self.A_train.shape[1]
+                app_row = self.A_train.shape[0]
+                api_col = self.A_train.shape[1]
                 sentence = ''
-                app = np.random.choice(api_row)
+                app = np.random.choice(app_row)
                 api = np.random.choice(np.nonzero(self.A_train[app,:])[1])
                 sentence += f'app{app} api{api} ' 
                 cur = 'api'
@@ -55,29 +55,34 @@ class Node2Vec():
                 sentence += f'app{end_app}' 
                 sentences.append(sentence)
         else:
-            self.B_train = sparse.load_npz(self.mat_dict['B']).tocsr()
-            self.P_train = sparse.load_npz(self.mat_dict['P']).tocsr()
-            self.H_matrix = (self.B_train+self.P_train).astype(bool).astype(int)
+            self.app_row = self.A_train.shape[0]
+            self.matrix = sparse.vstack([self.A_train,(sparse.load_npz(self.mat_dict['B']).tocsr()+sparse.load_npz(self.mat_dict['P']).tocsr())])
+            self.matrix = sparse.hstack([sparse.vstack([sparse.csr_matrix((self.app_row,self.app_row), dtype=np.int8),self.A_train.T]),self.matrix]).astype(bool).astype(int).tocsr()
+            self.matrix.setdiag(0)
+            self.potential_list = np.arange(self.matrix.shape[0])
             for _ in tqdm(range(self.num_sentences)):
                 sentence_len = np.random.choice(self.num_tokens)
-                api_row = self.A_train.shape[0]
-                app_col = self.A_train.shape[1]
                 sentence = ''
-                prev = 'app'+str(np.random.choice(api_row))
-                curr = 'api'+str(np.random.choice(np.nonzero(self.A_train[int(prev[3:]),:])[1]))
-                sentence += f'{prev} {curr} ' 
-                curr_type = 'api'
-                prev_type = 'app'
+                prev = np.random.choice(self.app_row)
+                curr = np.random.choice(np.nonzero(self.matrix[prev,:])[1])
+                sentence += f'app{prev} api{curr} ' 
                 for i in range(sentence_len):
-                    curr_type, prev_type,curr,prev = self.generate_with_probability(curr_type, prev_type,curr,prev)
-                    sentence += f'{curr} '
+                    curr,prev = self.generate_with_probability(curr,prev)
+                    sentence += f'{self.convert_to_string(curr)} '
                 sentence = sentence[:-1]
                 sentences.append(sentence)
         corpus = MyCorpus(sentences)
         model = gensim.models.Word2Vec(sentences=corpus, size=self.vec_size, min_count=1)
-        model.save(f'{self.output_path}/node2vec_all_5.model')
+        model.save(f'{self.output_path}/node2vec_concat_50000.model')
         print('saved')
     
+    def convert_to_string(self,row_num):
+        if row_num < self.app_row:
+            return 'app'+str(row_num)
+        else:
+            return 'api'+str(row_num - self.app_row)
+
+
     def A_only_generate_with_probability(self,orig_app, orig_api,start='app'):
         if start == 'app':
             api_list = np.nonzero(self.A_train[orig_app,:])[1]
@@ -92,98 +97,17 @@ class Node2Vec():
             prob_list = [i/total_prob for i in prob_list]
             return np.random.choice(app_list,p=prob_list)
 
-    def generate_with_probability(self,curr_type,prev_type,curr,prev):
-            if curr_type == 'app':
-                # api-> app -> api 
-                curr_list = self.A_train[int(curr[3:]),:].toarray()[0]
-                prev_list = self.H_matrix[int(prev[3:]),:].toarray()[0]
-                sum_list = np.sum([curr_list, prev_list], axis=0)
-                prob_list = []
-                potential_list = []
-                for ind,val in enumerate(sum_list):
-                    potential_list.append('api'+str(ind))
-                    if 'api'+str(ind) == prev:
-                        prob_list.append(1/self.p)
-                    elif 'api'+str(ind) == curr:
-                        prob_list.append(0)
-                    elif val == 2:
-                        prob_list.append(1)
-                    elif val == 1: 
-                        prob_list.append(1/self.q)
-                    else:
-                        prob_list.append(0)
-                    
-                total_prob = sum(prob_list)
-                prob_list = [i/total_prob for i in prob_list]
-                return_val = np.random.choice(potential_list,p = prob_list)
-                return 'api','app',return_val, curr
-            else:
-                if prev_type == 'app':
-                    # app -> api -> api & app-> api -> app
-                    # app -> api -> app
-                    potential_list = np.nonzero(self.A_train[:,int(curr[3:])])[0]
-                    prob_list = [1/self.p if 'app'+str(i)==prev else 1/self.q for i in potential_list]
-                    potential_list = ['app'+str(i) for i in potential_list]
-                    # app -> api -> api 
-                    curr_list = self.H_matrix[int(curr[3:]),:].toarray()[0]
-                    prev_list = self.A_train[int(prev[3:]),:].toarray()[0]
-                    sum_list = np.sum([curr_list,prev_list], axis=0)
-                    for ind, val in enumerate(sum_list):
-                        potential_list.append('api'+str(ind))
-                        if 'api'+str(ind) == prev:
-                            prob_list.append(1/self.p)
-                        elif 'api'+str(ind) == curr:
-                            prob_list.append(0)
-                        elif val == 2:
-                            prob_list.append(1)
-                        elif val == 1: 
-                            prob_list.append(1/self.q)
-                        else:
-                            prob_list.append(0)
-                    total_prob = sum(prob_list)
-                    prob_list = [i/total_prob for i in prob_list]
-                    return_val = np.random.choice(potential_list,p = prob_list)
-                    return return_val[:3],'api',return_val, curr
-                else: 
-                    # api -> api -> api, api -> api -> app
-                    potential_list = []
-                    prob_list = []
-                    # api -> api -> api 
-                    curr_list = self.H_matrix[int(curr[3:]),:].toarray()[0]
-                    prev_list = self.H_matrix[int(prev[3:]),:].toarray()[0]
-                    sum_list = np.sum([curr_list,prev_list],axis=0)
-                    for ind, val in enumerate(sum_list):
-                        potential_list.append('api'+str(ind))
-                        if 'api'+str(ind) == prev:
-                            prob_list.append(1/self.p)
-                        elif 'api'+str(ind) == curr:
-                            prob_list.append(0)
-                        elif val == 2:
-                            prob_list.append(1)
-                        elif val == 1: 
-                            prob_list.append(1/self.q)
-                        else:
-                            prob_list.append(0)
-                    # api -> api -> app
-                    curr_list = self.A_train[:,int(curr[3:])].toarray()[0]
-                    prev_list = self.A_train[:,int(prev[3:])].toarray()[0]
-                    sum_list = np.sum([curr_list, prev_list], axis=0)
-                    for ind, val in enumerate(sum_list):
-                        potential_list.append('api'+str(ind))
-                        if 'app'+str(ind) == prev:
-                            prob_list.append(1/self.p)
-                        elif 'app'+str(ind) == curr:
-                            prob_list.append(0)
-                        elif val == 2:
-                            prob_list.append(1)
-                        elif val == 1: 
-                            prob_list.append(1/self.q)
-                        else:
-                            prob_list.append(0)
-                    total_prob = sum(prob_list)
-                    prob_list = [i/total_prob for i in prob_list]
-                    return_val = np.random.choice(potential_list,p = prob_list)
-                    return return_val[:3],'api',return_val, curr
+    def generate_with_probability(self,curr,prev):
+        curr_list = np.nonzero(self.matrix[curr,:])[1]
+        prob_list = self.matrix[prev,:].toarray()[0]
+        prob_list[prev] = 1/self.p
+        prob_list = prob_list[curr_list]
+        prob_list = np.where(prob_list==0,1/self.q,prob_list)
+        total_prob = sum(prob_list)            
+        prob_list = [i/total_prob for i in prob_list]
+        return_val = np.random.choice(curr_list,p = prob_list)
+        return return_val, curr
+            
 
 def build_n2v(label_path, mat_dict, num_sentences, num_tokens, vec_size, output_path,p,q):
     node2vec = Node2Vec(label_path, mat_dict, num_sentences, num_tokens, vec_size, output_path,p,q)
